98907d61ef437b99b343fbf9f490886455323b5a
Elkin Garcia <elkingarcia.11@gmail.com>
2025-07-16 23:51:50 -0400

    initial commit

---
+++ trade_time_backtester.py
@@ -0,0 +1,3 @@
+# Test different trade range combinations
+# EX: Only open trades after 10:00am, 10:15am, 10:30am, 11am, 11:30am, 12pm, 12:30pm, 1pm, 1:30pm, 2pm, 2:30pm, 3pm, 3:30pm, 4pm
+# EX: Close all trades by 2:30pm, 2:45pm, 3pm, 3:15pm, 3:30pm, 3:45pm, 4pm
